171954aa56639bc634738b7d6ef070d19795bb07
Fiona Julieta Errat <fionajulietaa@gmail.com>
2024-01-03 22:39:52 +0700

    initial commit

--- src/test/java/StepDef/ShopCartStep.java
+++ src/test/java/StepDef/ShopCartStep.java
@@ -4,9 +4,7 @@ import Helper.Utility;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.*;
 
